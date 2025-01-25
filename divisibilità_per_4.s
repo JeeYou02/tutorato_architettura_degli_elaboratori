baeacaaadeed81c658e46dbe80aaa995aa522a89
JeeYou02 <rsngli02a20c351h@studium.unict.it>
2025-01-25 15:23:43 +0100

    Lezione 25/01/2025

---
+++ divisibilità_per_4.s
@@ -0,0 +1,20 @@
+ARR_IN  DCD     1,2,4,8,6,12
+LEN     DCD     6
+ARR_OUT FILL    6*4
+
+        MOV     R0, #ARR_IN ;puntatore array input
+        MOV     R1, #LEN
+        LDR     R1, [R1] ;lunghezza array
+        MOV     R2, #ARR_OUT ;puntatore array output
+
+ciclo   SUBS    R1, R1, #1
+        BLT     fine
+        LDR     R3, [R0], #4 ;elemento corrente
+        LSL     R4, R3, #30
+        CMP     R4, #0
+        BNE     ciclo
+        STR     R3, [R2], #4
+        B       ciclo
+
+fine    END
+
---
+++ somma_array.s
@@ -0,0 +1,19 @@
+ARR    DCD     1,3,5,2,7,6
+LEN    DCD     6
+SOMMA  FILL    4
+
+       MOV     R0, #ARR ;puntatore array
+       MOV     R1, #LEN
+       LDR     R1, [R1] ;dimensione array
+       MOV     R2, #0 ;somma parziale
+
+ciclo  SUBS    R1, R1, #1
+       BLT     fine
+       LDR     R3, [R0], #4 ;elemento corrente
+       ADD     R2, R2, R3
+       B       ciclo
+
+
+fine   MOV     R0, #SOMMA
+       STR     R2, [R0]
+       END
